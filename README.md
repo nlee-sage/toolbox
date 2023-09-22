a042fc3222dff3961b3a54b1dfe7669b4cefaac6
Nicholas Lee <126523251+nlee-sage@users.noreply.github.com>
2023-09-22 12:48:31 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# template_repo
+This repository is meant for using as a starting place for future repos
---
+++ _templates/py_script_boilerplate.py
@@ -0,0 +1,21 @@
+"""
+Name: 
+Description: 
+Contributors: Nicholas Lee
+"""
+# load modules
+import os
+import numpy as np
+import pandas as pd
+import yaml
+
+with open("../configs/_config.yml", "r") as f:
+    config = yaml.safe_load(f)
+
+def main():
+    print("Main")
+    return None
+
+
+if __name__ == "__main__":
+    main()
